d27658bbc7b5b019bd0d4a8b9ae7aaca5a9107ba
showzeng <694891221@qq.com>
2016-11-12 21:21:30 +0800

    fixed many bug

--- assets/js/post.js
+++ assets/js/post.js
@@ -7,17 +7,3 @@ window.onscroll = function() {
         document.getElementById('up-to-top').className = 'up-to-top'
     }
 }
-
-var hiddenProperty = 'hidden' in document ? 'hidden' :
-    'webkitHidden' in document ? 'webkitHidden' :
-    'mozHidden' in document ? 'mozHidden' :
-    null;
-var visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');
-var onVisibilityChange = function() {
-    if (!document[hiddenProperty]) {
-        document.title = "ˇε ˇ {{ page.title }}"
-    } else {
-        document.title = ":-( 不要离开我"
-    }
-}
-document.addEventListener(visibilityChangeEvent, onVisibilityChange);
